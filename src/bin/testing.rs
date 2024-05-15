b3b15d5ef51a079ad67777643db9f45f82f2ba66
roG0d <rodgarcas98@gmail.com>
2024-05-15 22:20:19 +0200

    Candle IndexOp i testing

--- src/bin/testing.rs
+++ src/bin/testing.rs
@@ -1,7 +1,7 @@
 #[path = "../env_runtime.rs"]
 mod env_runtime;
 
-use candle_core::{Device, Result, Tensor};
+use candle_core::{Device, IndexOp, Result, Tensor};
 use candle_nn::{embedding, loss::cross_entropy, Embedding, Module, VarBuilder, VarMap, ops::softmax};
 use ndarray::Array3;
 use std::env;
@@ -34,22 +34,36 @@ fn main(){
     println!("get dim 1 index 0: {:?}", d2_tensor.get_on_dim(1, 0).unwrap());
     println!("get dim 0 index 0 first element: {:?}", d2_tensor.get_on_dim(1, 0).unwrap().get(0).unwrap());
 
-    // i operator
+    /* i operator: Return a slice given an index.
+      Given a 3x4 Tensor:
+    - if the index is a scalar, it will return the row[index] Ej: i=0 -> [1.0, 2.0]
+    - if the index is a tuple, it will return the row[column[index]] Ej: i=(0,0) -> 1.0
+    */
+    let d2_data: Vec<f32> = (1..=12).map(|x| x as f32).collect();
+    let d2_tensor = Tensor::from_vec(d2_data, (3,4), &env_runtime.device).unwrap();
+    let d2i = d2_tensor.i(1).unwrap();
+    println!("Tensor2d i op: {:?}", d2i.to_vec1::<f32>());
+    /* Given a 3x2x2 Tensor: 
+    - if the index is a scalar, it will return the matrix[index] Ej: i=0 -> [[1.0, 2.0], [2.0, 3.0]]
+    - if the index is a tuple, it will return the matrix[row[index]] Ej: i=(0,0) -> [1.0, 2.0]
+    - if the index is a 3-tuple, it will reutn the matrix[row[column[index]]] Ej: i=(0,0,0) -> 1.0
+    */
     let d3_data: Vec<f32> = (1..=12).map(|x| x as f32).collect();
-    println!("Array3d data: {:?}", d3_data);
-    let d3_array = Array3::from_shape_vec((3, 2, 2), d3_data).unwrap();
-    println!("Array3d: {:?}", d3_array);
-    // let d3_tensor = 
-    // let d3i = d3_ar.i((0, generated_ids_cond_length - 1, ..))?;
+    //println!("Tensord 3d data: {:?}", d3_data);
+    let d3_tensor = Tensor::from_vec(d3_data, (3,2,2), &env_runtime.device).unwrap();
+
+    let d3i = d3_tensor.i((0,1,0)).unwrap();
+    println!("Tensor3d i op: {:?}", d3i.to_vec0::<f32>());
+    
     // Softmax testing
-    println!("get dim 0 index 0 vector: {:?}", d2_tensor.get_on_dim(0, 0).unwrap().to_vec1::<f32>().unwrap());
+    // println!("get dim 0 index 0 vector: {:?}", d2_tensor.get_on_dim(0, 0).unwrap().to_vec1::<f32>().unwrap());
     let probs = softmax(&d2_tensor, 1).unwrap();
     // Sofmax creates a  probability distribution from 0 to 1 with every element dim-wise -> in a Tensor(2,2) dim=0 column-wise, dim=1 row-wise
-    println!("get dim 0 index 0 vector after softmax: {:?}", probs.get_on_dim(0, 0).unwrap().to_vec1::<f32>().unwrap());
+    // println!("get dim 0 index 0 vector after softmax: {:?}", probs.get_on_dim(0, 0).unwrap().to_vec1::<f32>().unwrap());
 
     //Ndarray
     let d3_data: Vec<f32> = (1..=12).map(|x| x as f32).collect();
-    println!("Array3d data: {:?}", d3_data);
+    // println!("Array3d data: {:?}", d3_data);
     let d3_array = Array3::from_shape_vec((3, 2, 2), d3_data).unwrap();
     println!("Array3d: {:?}", d3_array);
 }
